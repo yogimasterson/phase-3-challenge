a2ff6239e10f54692251e4198df550e7707e8551
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-07-25 16:07:41 -0700

    finished all specks

--- part-1/app.js
+++ part-1/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 
 const app = express()
 app.use(bodyParser.json())
-app.use( bodyParser.urlencoded({ extended: true }))
+app.use( bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
 	res.send('Welcome!!!<br>Please use the following routes:<br>/api/supported-operations<br>/api/square<br>/api/compute')
@@ -25,6 +25,35 @@ app.get('/api/square', (req, res) => {
 		"result": num * num
 	})
 	res.send(json)
+})
+
+
+app.get('/api/square', (req, res) => {
+  const num = req.query.number
+  res.send({"result": num * num})
+})
+
+app.post('/api/compute', (req, res) => {
+  const operator = req.body.operator
+	const operands = req.body.operands
+	console.log(operator)
+
+  if (operator === '-') {
+    res.send({"result": operands[0] - operands[1]})
+  }
+  if (operator === '*') {
+    res.send({"result": operands[0] * operands[1]})
+  }
+  if (operator === '/') {
+    res.send({"result": operands[0] / operands[1]})
+  }
+  if (operator === '+') {
+    res.send({"result": parseInt(operands[0]) + parseInt(operands[1]) })
+  }
+  else {
+    res.status(404).send({"error": "invalid operator ?. Valid operators are /, +, -, *"})
+  }
+
 })
 
 app.listen(3000, () => {
